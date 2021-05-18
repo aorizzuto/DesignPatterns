5b512a44bf67a7d96d0430b18a17e835dc7d17ca
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-05-18 14:52:35 -0300

    Composite pattern

--- Structural/Composite/BinaryFile.java
+++ Structural/Composite/BinaryFile.java
@@ -1,32 +1,33 @@
 package Structural.Composite;
 
 public class BinaryFile extends File {
-    private long size;
+	
+	private long size;
+	
+	public BinaryFile(String name, long size) {
+		super(name);
+		this.size = size;
+	}
 
-    public BinaryFile(String name, long size){
-        super(name);
-        this.size = size;
-    }
+	@Override
+	public void ls() {
+		System.out.println(getName() +"\t"+size);
+	}
 
-    @Override
-    public void ls() {
-        System.out.println(getName() + "\t" + size);        
-    }
+	@Override
+	public void addFile(File file) {
+		throw new UnsupportedOperationException("Leaf node doesn't support add operation");
+	}
 
-    @Override
-    public void addFile(File file) {
-        throw new UnsupportedOperationException("Leaf node does not support add operation");
-    }
+	@Override
+	public File[] getFiles() {
+		throw new UnsupportedOperationException("Leaf node doesn't support get operation");
+	}
 
-    @Override
-    public File[] getFiles() {
-        throw new UnsupportedOperationException("Leaf node does not support get operation");
-    }
-
-    @Override
-    public boolean removeFile(File file) {
-        throw new UnsupportedOperationException("Leaf node does not support remove operation");
-    }
-
-    
+	@Override
+	public boolean removeFile(File file) {
+		throw new UnsupportedOperationException("Leaf node doesn't support remove operation");
+	}
+	
+	
 }
--- Structural/Composite/File.java
+++ Structural/Composite/File.java
@@ -2,22 +2,26 @@ package Structural.Composite;
 
 // Component base class for composite pattern
 public abstract class File {
-    
-    private String name;
 
-    public File(String name) {
-        this.name = name;
-    }
+	private String name;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+	public File(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
-    public abstract void ls();
-    public abstract void addFile(File file);
-    public abstract File[] getFiles();
-    public abstract boolean removeFile(File file);
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public abstract void ls();
+	
+	public abstract void addFile(File file);
+
+	public abstract File[] getFiles();
+	
+	public abstract boolean removeFile(File file);
 }
