5af05992edfdc443e9c81f6513585a755cea88d8
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-05-19 18:20:22 -0300

    proxy design pattern

---
+++ Structural/Proxy/dynamic/Client.java
@@ -0,0 +1,17 @@
+package Structural.Proxy.dynamic;
+
+import Structural.Proxy.Image;
+
+import javafx.geometry.Point2D;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Image img = ImageFactory.getImage("A.bmp");
+		img.setLocation(new Point2D(-10, 0));
+		System.out.println(img.getLocation());
+		System.out.println("*****************************");
+		img.render();
+		
+	}
+}
