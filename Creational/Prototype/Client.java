f8b52ed994c871322617275761a376a1a3743281
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-05-23 18:49:46 -0300

    prototype pattern

--- Creational/Prototype/Client.java
+++ Creational/Prototype/Client.java
@@ -1,17 +1,17 @@
 package Creational.Prototype;
 
-import javafx.geometry.Point3D;
-
 public class Client {
     
     public static void main(String[] args) {
-        Swordsman s1 = new Swordsman();
-        s1.move(new Point3D(-10,0,0),20);
-        s1.atack();
-
-        System.out.println(s1);
-
-        Swordsman s2 = (Swordsman) s1.clone();
-        System.out.println(s2);
-    }
+        ShapeCache.loadCache();
+  
+        Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());		
+  
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());		
+  
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());		
+     }
 }
